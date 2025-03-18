f3594c6416f4994d8f0e1747002571e58f139e08
vbludov <ext-viktor.bludov@flowbird.group>
2025-03-18 15:57:07 +0200

    added skeleton for POM

---
+++ pages/pageManager.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+import { DynamicIdPage } from "./dynamicIdPage";
+
+export class PageManager {
+  private readonly page: Page;
+  private readonly dynamicIdPage: DynamicIdPage;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.dynamicIdPage = new DynamicIdPage(this.page);
+  }
+
+  onDynamicIdPage() {
+    return this.dynamicIdPage;
+  }
+}
