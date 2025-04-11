88f01ae556c12d3c794953a4f1caa356e61efbcd
vbludov <ext-viktor.bludov@flowbird.group>
2025-04-11 16:56:34 +0300

    concluded assertions in test cases

--- tests/basicTests.spec.ts
+++ tests/basicTests.spec.ts
@@ -33,13 +33,13 @@ test('hidden layers page @regression', async ({ page }) => {
 
     await homePage.openHiddenLayersPage()
     await hiddenLayersPage.clickGreenButton()
-    await expect(hiddenLayersPage.greenButtonDiv).toHaveCSS('z-index', '1')
-    await expect(hiddenLayersPage.blueButtonDiv).toHaveCSS('z-index', '2')
+    await expect(hiddenLayersPage.greenButtonDiv, 'Incorrect z-index of green button').toHaveCSS('z-index', '1')
+    await expect(hiddenLayersPage.blueButtonDiv, 'Incorrect z-index of blue button').toHaveCSS('z-index', '2')
 })
 
 test('load delay page', async ({ page }) => {
     const loadDelayPage = new LoadDelayPage(page)
 
     await homePage.openLoadDelayPage(10000)
-    await expect(loadDelayPage.delayedButton).toBeVisible()
+    await expect(loadDelayPage.delayedButton, 'Load delay button not visible').toBeVisible()
 })
